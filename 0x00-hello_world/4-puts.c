0963c0c5551b04177c7345f2ed3833634a652f6e
temple69 <utemple169@gmail.com>
2022-10-13 22:06:26 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
 /**
+ * main - This prints out the text when run
  *
- *  * main - This prints out the text when run
- *
- *   *
- *
- *    * Return: 0 without errors
- *
- *     */
-
-
+ * Return: 0 without errors
+ */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
