7369b9eeefdbd594cc96982a2a50f22ae060cfd7
temple69 <utemple169@gmail.com>
2022-10-19 21:40:54 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of an int
+ * @n : number to check
+ * Return: int
+ */
+
+int _abs(int n)
+{
+
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+
+
+}
